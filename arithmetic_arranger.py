bb5c8e30e0a301caf56e9931b056a4e002b19080
nicolasoleinizak-gj <nicolas.oleinizak@gojiraf.com>
2023-04-06 21:59:49 -0300

    feat: check for bad arguments

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,16 @@
 def arithmetic_arranger(problems):
 
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
+    
+    for problem in problems:
+        members = problem.split(' ')
+        if members[1] == '/' or members[1] == '*':
+            return 'Error: Operator must be \'+\' or \'-\'.'
+        if len(members[0])>4 or len(members[2])>4:
+            return 'Error: Numbers cannot be more than four digits.'
+        if not members[0].isnumeric() or not members[2].isnumeric():
+            return 'Error: Numbers must only contain digits.'
 
+    arranged_problems = ''
     return arranged_problems
